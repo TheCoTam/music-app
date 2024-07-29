953893e82cc522f76febd0b371ef0d54abf4c65a
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-07-29 22:57:36 +0700

    hoàn thành api lấy thông tin phòng theo mã phòng

--- server/api/views.py
+++ server/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, status
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Room
 from .serializers import RoomSerializer, CreateRoomSerializer
@@ -10,6 +10,26 @@ from .serializers import RoomSerializer, CreateRoomSerializer
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class GetRoom(APIView):
+    serializer_class = RoomSerializer
+    lookup_url_kwang = 'code'
+
+    def get(self, request, format=None):
+        code = request.GET.get(self.lookup_url_kwang)
+
+        if code != None:
+            room = Room.objects.filter(code=code)
+            if len(room) > 0:
+                data = RoomSerializer(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+
+                return Response(data, status=status.HTTP_200_OK)
+
+            return Response({'Room not found': 'Invalid Room Code.'}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response({'Bad request': 'Code parameter not found in request'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CreateRoomView(APIView):
